de60c4bff2c21f888c26c10491df5bff49c0a306
TwistedAlex <muzihuole@gmail.com>
2022-04-23 22:47:11 -0500

    Initial Django Project

--- provision.sh
+++ provision.sh
@@ -48,6 +48,27 @@ sudo mysql -u root << EOF
 EOF
 # fi
 
+# superuser名字
+USER="admin"
+# superuser密码
+PASS="admin"
+# superuser邮箱
+MAIL="admin@twitter.com"
+script="
+from django.contrib.auth.models import User;
+
+username = '$USER';
+password = '$PASS';
+email = '$MAIL';
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password);
+    print('Superuser created.');
+else:
+    print('Superuser creation skipped.');
+"
+printf "$script" | python manage.py shell
+
 
 # 如果想直接进入/vagrant路径下
 # 请输入vagrant ssh命令进入
