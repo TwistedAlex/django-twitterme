6b1448f761a57365f693013a2b7ccc010dcee4f0
TwistedAlex <muzihuole@gmail.com>
2022-05-09 16:02:52 -0500

    Comment create api and unit test

--- twitter/urls.py
+++ twitter/urls.py
@@ -13,15 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from accounts.api.views import UserViewSet, AccountViewSet
+from comments.api.views import CommentViewSet
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
-from django.conf import settings
-
-from accounts.api.views import UserViewSet, AccountViewSet
-from tweets.api.views import TweetViewSet
 from friendships.api.views import FriendshipViewSet
 from newsfeeds.api.views import NewsFeedViewSet
+from rest_framework import routers
+from tweets.api.views import TweetViewSet
 
 router = routers.DefaultRouter()
 router.register(r'api/users', UserViewSet)
@@ -29,6 +29,7 @@ router.register(r'api/accounts', AccountViewSet, basename='accounts')
 router.register(r'api/tweets', TweetViewSet, basename='tweets')
 router.register(r'api/friendships', FriendshipViewSet, basename='friendships')
 router.register(r'api/newsfeeds', NewsFeedViewSet, basename='newsfeeds')
+router.register(r'api/comments', CommentViewSet, basename='comments')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
