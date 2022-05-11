75c2adc87f2a9f4dd172c22e225e1bce41108012
TwistedAlex <muzihuole@gmail.com>
2022-05-11 10:34:45 -0500

    Add likes create api and unit test

--- testing/testcases.py
+++ testing/testcases.py
@@ -47,3 +47,8 @@ class TestCase(DjangoTestCase):
         )
         return instance
 
+    def create_user_and_client(self, *args, **kwargs):
+        user = self.create_user(*args, **kwargs)
+        client = APIClient()
+        client.force_authenticate(user)
+        return user, client
--- twitter/urls.py
+++ twitter/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from friendships.api.views import FriendshipViewSet
+from likes.api.views import LikeViewSet
 from newsfeeds.api.views import NewsFeedViewSet
 from rest_framework import routers
 from tweets.api.views import TweetViewSet
@@ -30,6 +31,7 @@ router.register(r'api/tweets', TweetViewSet, basename='tweets')
 router.register(r'api/friendships', FriendshipViewSet, basename='friendships')
 router.register(r'api/newsfeeds', NewsFeedViewSet, basename='newsfeeds')
 router.register(r'api/comments', CommentViewSet, basename='comments')
+router.register(r'api/likes', LikeViewSet, basename='likes')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
