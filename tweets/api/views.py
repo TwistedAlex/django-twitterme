d2446b5cfdcd8cf8a8e4505eaf2af9b6f67feb4b
TwistedAlex <muzihuole@gmail.com>
2022-05-13 21:39:11 -0500

    add endless pagination module for tweets list api

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -1,3 +1,4 @@
+from newsfeeds.services import NewsFeedService
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
@@ -7,8 +8,8 @@ from tweets.api.serializers import (
     TweetSerializerForDetail,
 )
 from tweets.models import Tweet
-from newsfeeds.services import NewsFeedService
 from utils.decorators import required_params
+from utils.paginations import EndlessPagination
 
 
 class TweetViewSet(viewsets.GenericViewSet,
@@ -20,6 +21,7 @@ class TweetViewSet(viewsets.GenericViewSet,
     # self.get_queryset() will execute the queryset ORM;
     queryset = Tweet.objects.all()
     serializer_class = TweetSerializerForCreate
+    pagination_class = EndlessPagination
 
     def get_permissions(self):
         """
@@ -62,6 +64,7 @@ class TweetViewSet(viewsets.GenericViewSet,
         tweets = Tweet.objects.filter(
             user_id=user_id
         ).prefetch_related('user').order_by('-created_at')
+        tweets = self.paginate_queryset(tweets)
         serializer = TweetSerializer(
             tweets,
             context={'request': request},
@@ -69,7 +72,7 @@ class TweetViewSet(viewsets.GenericViewSet,
         )
         # 一般来说 json 格式的 response 默认都要用 hash 的格式
         # 而不能用 list 的格式（约定俗成）
-        return Response({'tweets': serializer.data})
+        return self.get_paginated_response(serializer.data)
 
     def create(self, request, *args, **kwargs):
         """
