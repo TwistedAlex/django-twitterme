6d7c79229efa844f9230f6f1485a6ce7c3579431
TwistedAlex <muzihuole@gmail.com>
2022-05-16 23:48:39 -0500

    hbase testing

--- testing/testcases.py
+++ testing/testcases.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.core.cache import caches
 from django.test import TestCase as DjangoTestCase
+from django_hbase.models import HBaseModel
 from friendships.models import Friendship
 from likes.models import Like
 from newsfeeds.models import NewsFeed
@@ -12,6 +13,23 @@ from utils.redis_client import RedisClient
 
 
 class TestCase(DjangoTestCase):
+    hbase_tables_created = False
+
+    def setUp(self):
+        self.clear_cache()
+        try:
+            self.hbase_tables_created = True
+            for hbase_model_class in HBaseModel.__subclasses__():
+                hbase_model_class.create_table()
+        except Exception:
+            self.tearDown()
+            raise
+
+    def tearDown(self):
+        if not self.hbase_tables_created:
+            return
+        for hbase_model_class in HBaseModel.__subclasses__():
+            hbase_model_class.drop_table()
 
     def clear_cache(self):
         RedisClient.clear()
